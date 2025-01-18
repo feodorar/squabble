c83a731b9657f339086ad75c51c8dbf4393d04e3
Feodora Reis <feodora.reis@posteo.de>
2025-01-18 15:52:05 +0100

    first attempt to set up push notifications

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,29 @@
+<script lang="ts">
+	import { prisma } from '$lib/db';
+	import OneSignal from '@nolanx/svelte-onesignal';
+
+	OneSignal.init({
+		appId: '1a853b4d-510b-4741-b642-4136cce7683e',
+		notifyButton: {
+			enable: true
+		},
+		allowLocalhostAsSecureOrigin: true
+	});
+
+	OneSignal.on('subscriptionChange', async (isSubscribed) => {
+		if (isSubscribed) {
+			const token = await OneSignal.getUserId();
+			// Save token to Supabase
+			saveDeviceToken(token, 'web');
+		}
+	});
+
+	async function saveDeviceToken(token: string | null | undefined, platform: string) {
+		if (token) {
+			console.log('token', token);
+			prisma.device_tokens.create({ data: { token, platform, user_id: 'test' } });
+		}
+	}
+</script>
+
 <div class="m-4 flex flex-col items-center justify-center">Hello</div>
